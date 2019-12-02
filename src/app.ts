cbb11424bc545c71d5f32e2ae31b0f21fc6e6b65
Srinivas Reddy Thatiparthy <thatiparthysreenivas@gmail.com>
2019-12-02 19:48:58 +0530

    Add companies JSON interfaces and typesafety to crawler

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,7 @@ import {Crawler} from "./companies";
 async function start() {
     const url = "https://content.fortune.com/wp-json/irving/v1/data/franchise-search-results?list_id=2611932";
     const crawler = new Crawler(url);
-    await crawler.generateCSV();
+    await crawler.generateCSVFile();
 }
 
 start();
--- src/companies.ts
+++ src/companies.ts
@@ -1,5 +1,49 @@
-import { ExportToCsv } from "export-to-csv";
+import ObjectsToCsv from "objects-to-csv";
 import puppeteer from "puppeteer";
+
+interface ISortableObject {
+    description: string;
+    importField: string;
+    order: string;
+    saveIn: string;
+    sortable: string;
+    title: string;
+    type: string;
+}
+
+interface IFilterableObject {
+    filterable: string;
+    importField: string;
+    options: string [] ;
+    saveIn: string;
+    title: string;
+    type: string;
+}
+
+interface IFirstObject {
+    filterable: IFilterableObject;
+    key: string;
+    sortable: ISortableObject [];
+}
+
+interface IFieldsObject {
+    key: string;
+    value: string;
+}
+interface IItemsObject {
+    fields: IFieldsObject [];
+    permalink: string;
+}
+
+interface ISecondObject {
+    key: string;
+    items: IItemsObject [];
+}
+
+interface ICsvObject {
+    companyName: string;
+    url: string;
+}
 
 export class Crawler {
     private url: string;
@@ -12,7 +56,7 @@ export class Crawler {
         this.results = [];
     }
 
-    public async run() {
+    public async run(): Promise<[IFirstObject, ISecondObject]> {
         const browser = await puppeteer.launch();
         const page = await browser.newPage();
         const url: string = this.url;
@@ -21,27 +65,43 @@ export class Crawler {
         do {
             await page.goto(url);
             results = await page.evaluate(() => {
-                return JSON.parse(document.querySelector("body").innerText);
+                return JSON.parse((document.querySelector("body") as HTMLBodyElement).innerText);
             });
             console.log(results);
             this.results.push.apply(this.results, results);
         }while (results.results && results.results.length > 0);
         browser.close();
-
+        return results;
     }
 
-    public async generateCSV() {
-        const result = await this.run();
-        const options = {
-            fieldSeparator: ",",
-            showLabels: true,
-            showTitle: true,
-            title: "fortune-500",
-            useBom: true,
-            useKeysAsHeaders: true,
-            useTextFile: false,
-        };
-        const csv = new ExportToCsv(options);
-        csv.generateCsv(result);
+    public async generateCSVObjectList(): Promise<ICsvObject[]> {
+        const results: [IFirstObject, ISecondObject] = await this.run();
+        const result: ISecondObject = results[1];
+        const items: IItemsObject [] = result.items;
+        const csvObjectList: ICsvObject [] = [];
+        for (const item of items) {
+            const field: IFieldsObject = item.fields.find((obj: IFieldsObject) => obj.key === "name")!;
+            const permalink: string = item.permalink;
+            csvObjectList.push({companyName: field.value, url: permalink});
+        }
+        return csvObjectList;
+    }
+
+    public async generateCSVFile() {
+        const newCsvList: ICsvObject [] = [];
+        const csvArray: ICsvObject [] =  await this.generateCSVObjectList();
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+        for (const csvObject of csvArray) {
+            await page.goto(csvObject.url);
+            page.waitFor(this.waitFor);
+            const url = await page.evaluate(() => {
+                return (document.querySelector("table tbody tr td a") as HTMLAnchorElement).href;
+            });
+            newCsvList.push({companyName: csvObject.companyName, url});
+        }
+        const csv = new ObjectsToCsv(newCsvList);
+        await csv.toDisk("./test.csv");
+        await browser.close();
     }
 }
