d7c5465b0e6265f2d660886e8fff2a275fad454e
Srinivas Reddy Thatiparthy <thatiparthysreenivas@gmail.com>
2019-12-04 15:36:40 +0530

    Remove extranous function

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,4 @@
 
 import { Crawler } from "./companies";
 
-async function start() {
-    const crawler = new Crawler(2019, "./srinivas.csv");
-    await crawler.generateCSVFile();
-}
-start();
-
 export * from "./companies";
